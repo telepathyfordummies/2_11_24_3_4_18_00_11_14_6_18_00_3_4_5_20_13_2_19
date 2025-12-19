35c33aedfaa1c77515050939b547c929a587a973
telepathyfordummies <telepathyfordummies@gmail.com>
2025-12-19 18:27:59 -0500

    Update clocks.js

--- js/clocks.js
+++ js/clocks.js
@@ -1,15 +1,15 @@
 const clockField = document.getElementById("clockField");
-
 const CLOCK_COUNT = 9;
 
 for (let i = 1; i <= CLOCK_COUNT; i++) {
   const id = String(i).padStart(2, "0");
-
   const hasPendulum = i === 1;
 
-  const clock = document.createElement("a");
-  clock.href = "#"; // placeholder redirect
-  clock.className = "clock-unit";
+  const link = document.createElement("a");
+  link.href = "#";
+
+  const clock = document.createElement("div");
+  clock.className = `clock-unit ${hasPendulum ? "has-pendulum" : ""}`;
 
   clock.innerHTML = `
     <img src="images/clocks/clock${id}_face.png" class="clock-face">
@@ -18,7 +18,6 @@ for (let i = 1; i <= CLOCK_COUNT; i++) {
     ${hasPendulum ? `<img src="images/clocks/clock${id}_pendulum.png" class="pendulum">` : ""}
   `;
 
-  clock.className = `clock-unit ${hasPendulum ? "has-pendulum" : ""}`;
-
-  clockField.appendChild(clock);
+  link.appendChild(clock);
+  clockField.appendChild(link);
 }
