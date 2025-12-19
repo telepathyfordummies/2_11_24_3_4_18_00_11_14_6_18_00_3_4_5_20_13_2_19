3b83408a24bb32552d17a4d3f2d8dcdc5db7c86a
telepathyfordummies <telepathyfordummies@gmail.com>
2025-12-19 17:58:10 -0500

    Create clocks.js

---
+++ js/clocks.js
@@ -0,0 +1,22 @@
+const clockField = document.getElementById("clockField");
+
+const CLOCK_COUNT = 9;
+
+for (let i = 1; i <= CLOCK_COUNT; i++) {
+  const id = String(i).padStart(2, "0");
+
+  const hasPendulum = i === 1;
+
+  const clock = document.createElement("a");
+  clock.href = "#"; // placeholder redirect
+  clock.className = "clock-unit";
+
+  clock.innerHTML = `
+    <img src="images/clocks/clock${id}_face.png" class="clock-face">
+    <img src="images/clocks/clock${id}_hour.png" class="hand hour">
+    <img src="images/clocks/clock${id}_minute.png" class="hand minute">
+    ${hasPendulum ? `<img src="images/clocks/clock${id}_pendulum.png" class="pendulum">` : ""}
+  `;
+
+  clockField.appendChild(clock);
+}
