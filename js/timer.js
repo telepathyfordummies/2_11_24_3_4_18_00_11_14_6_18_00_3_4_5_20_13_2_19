114fd7a14c393aacb8086f2ee208edbf1e1d3242
telepathyfordummies <telepathyfordummies@gmail.com>
2025-12-20 02:59:53 -0500

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,6 @@
-const REDIRECT_TIME = (3 * 60 + 33) * 1000; // 3:33 in ms
+// Redirect after 3:33
+const REDIRECT_TIME = 5000;
 
 setTimeout(() => {
-  window.location.href = "log01.html";
+  window.location.href = "logs/log01.html"; // first log
 }, REDIRECT_TIME);
