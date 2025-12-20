77bd0f4bf4a81190d524c3597e5b6bf215b7680e
telepathyfordummies <telepathyfordummies@gmail.com>
2025-12-20 03:07:52 -0500

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,6 @@
-const REDIRECT_TIME = 5000; // 5 seconds for testing
-
-setTimeout(() => {
-  window.location.href = "logs/log01.html"; // adjust path if needed
-}, REDIRECT_TIME);
+document.addEventListener("DOMContentLoaded", () => {
+  const REDIRECT_TIME = 5000; // 5 seconds for testing
+  setTimeout(() => {
+    window.location.href = "logs/log01.html";
+  }, REDIRECT_TIME);
+});
